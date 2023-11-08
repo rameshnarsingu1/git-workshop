781926a0c293597dbac3ca60b8f24aefc4511fd0
Ramesh Narsingu <narsingu.royal@gmail.com>
2023-11-08 14:38:35 +0530

    modified java coding

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("This is Ramesh Narsingu learning Jenkins");
+    System.out.println("Suresh watching my code");
   }
 }
